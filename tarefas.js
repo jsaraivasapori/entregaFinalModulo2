3f800a9a50f877079e09be0b69993078ab8db651
Priscila de Lima Pereira <priscila.lima@ibm.com>
2024-02-18 16:13:11 -0300

    #Update para validação em menu

--- tarefas.js
+++ tarefas.js
@@ -1,18 +1,18 @@
 export const tarefas = [
   {
-    id: 0,
+    id: 1,
     nome: 'Fazer feira',
     descricao: '',
     concluido: false,
   },
   {
-    id: 1,
+    id: 2,
     nome: 'Limpar casa',
     descricao: '',
     concluido: false,
   },
   {
-    id: 2,
+    id: 3,
     nome: 'Teste',
     descricao: '',
     concluido: false,

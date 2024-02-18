f0a64d8390050e6e1d2bc485fa475a0f746cd565
João Vitor <joaosaraivasapori@gmail.com>
2024-02-18 14:49:45 -0300

    Resolvido problema de repetição de menu no console

--- editarTarefa.js
+++ editarTarefa.js
@@ -4,20 +4,21 @@ import { tarefas } from './tarefas.js';
 export function editarTarefa() {
   const idEditar = Number(prompt("Informe o ID da tarefa: ").trim())
 
- // Busca esse id no array tarefas
- let tarefaEncontrada = tarefas.find(tarefa =>  tarefa.id === idEditar)
+ 
+  let tarefaEncontrada = tarefas.find(tarefa =>  tarefa.id === idEditar)
 
- if (!tarefaEncontrada) {
-  console.log('Tarefa não encontrada.Repita a operação');
-  return tarefas;
-}
- const menuEdicao = Number(prompt(
-  `
-   1 - Alterar nome da tarefa
-   2 - Alterar Descrição da tarefa
-   3 - Alterar status da tarefa 
-   Escolha o que deseja alterar na tarefa ${tarefaEncontrada.nome}: `
-  ).trim())
+  if (!tarefaEncontrada) {
+    console.log('Tarefa não encontrada.Repita a operação');
+    return tarefas;
+  }
+
+  console.log(`
+  1 - Alterar nome da tarefa
+  2 - Alterar Descrição da tarefa
+  3 - Alterar status da tarefa
+  `)
+  const menuEdicao = Number(prompt(`Escolha o que deseja alterar na tarefa *${tarefaEncontrada.nome}*:` 
+    ).trim())
 
  switch (menuEdicao){
   case 1:
@@ -25,6 +26,7 @@ export function editarTarefa() {
     const nomeAlterado = prompt("Informe novo nome: ").trim()
     tarefaEncontrada.nome = nomeAlterado
     console.log(`Alteração efetuada com sucesso. ${tarefaEncontrada.nome} alterado para ${nomeAlterado} `)
+
     break
 
     case 2:
@@ -32,31 +34,30 @@ export function editarTarefa() {
       const descricaoAlterada = prompt("Informe a nova descricao: ").trim()
       tarefaEncontrada.descricao = descricaoAlterada
       console.log(`Alteração efetuada com sucesso. ${tarefaEncontrada.descricao} alterado para ${descricaoAlterada}`)
+
       break
 
     case 3:
       let statusValido = false
       while(!statusValido){
-      console.clear()
-      const statusAlterado = Number(prompt(
-        `
+        console.clear()
+        console.log(`
         0- Tarefa em adamento
         1 - Tarefa concluída
-        
+        `)
+        const statusAlterado = Number(prompt(`Informe o novo status:`).trim())
+        if(statusAlterado === 0 || statusAlterado === 1){
 
-        Informe o novo status: `
-      ).trim())
-      if(statusAlterado === 0 || statusAlterado === 1){
+        statusValido = true
+        tarefaEncontrada.concluido = Boolean(statusAlterado)
 
-      statusValido = true
-      tarefaEncontrada.concluido = Boolean(statusAlterado)
-
-      console.log(`Alteração efetuada com sucesso. ${tarefaEncontrada.concluido} alterado para ${Boolean(statusAlterado)}`)
-      }
-      else(console.log("Opção inexistente, revise a ação"))
-    }break
+        console.log(`Alteração efetuada com sucesso.`)
+        }
+        else(console.log("Opção inexistente, revise a ação"))
+        break
+    }
+    break
     
-
     default:
       console.log("Algo deu errado, volte ao menu e refaça a operação");
  }

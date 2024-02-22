936e7935a558e7fef32ec348202b4d796fc57e10
famgz <famgz@proton.me>
2024-02-21 21:39:50 -0300

    refactor: remocao de codigo inutilizado

--- utils.js
+++ utils.js
@@ -1,4 +1,3 @@
-import { tarefas } from './tarefas.js';
 import promptSync from 'prompt-sync';
 export const prompt = promptSync();
 
@@ -6,9 +5,4 @@ export function obterIdValido(tarefas) {
   const ids = tarefas.map((tarefa) => tarefa.id).sort((a, b) => a - b);
   const lastId = ids.at(-1) || 0;
   return lastId + 1;
-}
-
-export function obterIdTarefa(tarefas) {
-  let id = null;
-  mostrarTarefas();
 }

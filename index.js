f133e3858570708229800af9168646076519ad8e
famgz <famgz@proton.me>
2024-02-21 21:31:51 -0300

    refactor: array tarefas dentro de index refactor: listar tarefas dentro do contexto das funcoes

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import { editarTarefa } from './editarTarefa.js';
 import { listarTarefas } from './listarTarefas.js';
 import { mostrarTarefa } from './mostrarTarefa.js';
 import { removerTarefa } from './removerTarefa.js';
-import { tarefas } from './tarefas.js';
 import { obterIdValido, prompt } from './utils.js';
 
-const tasks =  [...tarefas];
+const tarefas = [];
 
 const menu = [
   'Adicionar uma tarefa',
@@ -17,7 +16,7 @@ const menu = [
 ];
 
 function mostrarMenu() {
-  console.log("Organizador de tarefas\n")
+  console.log('Organizador de tarefas\n');
   for (let i in menu) {
     i = Number(i);
     console.log(`${i + 1}. ${menu[i]}`);
@@ -32,7 +31,7 @@ function iniciar() {
       console.clear();
       mostrarMenu();
       console.log();
-      
+
       escolha = Number(
         prompt('Qual opção deseja? [digite 0 para sair]: ').trim()?.[0]
       );
@@ -50,29 +49,25 @@ function iniciar() {
 
     let id = null;
     if (escolha === 3 || escolha === 4 || escolha === 5) {
-      id = obterIdValido(tasks);
+      id = obterIdValido(tarefas);
     }
 
     switch (escolha) {
       case 1:
-        adicionarTarefa(tasks);
+        adicionarTarefa(tarefas);
         break;
       case 2:
-        editarTarefa(tasks);
+        editarTarefa(tarefas);
         break;
       case 3:
-        removerTarefa(tasks);
+        removerTarefa(tarefas);
         break;
       case 4:
-        listarTarefas(tasks);
+        listarTarefas(tarefas);
         break;
       case 5:
-        mostrarTarefa(tasks);
+        mostrarTarefa(tarefas);
         break;
-    }
-
-    if (escolha !== 4) {
-      listarTarefas(tasks);
     }
 
     prompt('Aperte enter para continuar');
